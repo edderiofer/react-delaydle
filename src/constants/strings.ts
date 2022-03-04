9a5131258a1711a535717beec4b2f0eebd62e62b
edderiofer <edderiofer@hotmail.com>
2022-03-04 15:07:15 +0000

    Added string for delay message

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -4,6 +4,7 @@ export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const DELAY_MESSAGE = 'Waiting for response, make another guess in the meantime!'
 export const HARD_MODE_ALERT_MESSAGE =
   'Hard Mode can only be enabled at the start!'
 export const HARD_MODE_DESCRIPTION =
